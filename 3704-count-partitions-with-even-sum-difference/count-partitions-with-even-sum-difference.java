c88e06b2f2484335a2c89e87659970307eaa9b12
Bitu Kumar <152399673+Bitu1269@users.noreply.github.com>
2025-12-05 10:32:18 +0530

    Time: 1 ms (48.14%) | Memory: 43.2 MB (45.67%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
@@ -3,17 +3,16 @@ class Solution {
         int n= nums.length;
         int leftSum=0;
         int count = 0;
+        int total=0;
 
+        for(int num : nums){
+            total += num;
+        }
         for(int i=0; i<n-1; i++){
             leftSum += nums[i];
-            int rightSum=0;
-            for(int j=i+1; j<n; j++){
-                rightSum += nums[j];
-            }
-            int diff=leftSum-rightSum;
-            if(diff %2==0){
-                count++;
-            }
+            int rightSum = total - leftSum;
+            int diff=leftSum - rightSum;
+            if(diff % 2 == 0) count++;
         }
         return count;
     }
