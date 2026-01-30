d179e847759d34d5429ebbc876b82e0bfd3222d5
Bitu Kumar <152399673+Bitu1269@users.noreply.github.com>
2026-01-30 09:08:20 +0530

    Time: 2 ms (63.90%) | Memory: 42.8 MB (90.21%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkIfPangram(String s) {
+        boolean[] visit=new boolean[26];
+
+        for(int i=0; i<s.length(); i++){
+            char ch=s.charAt(i);
+            visit[ch - 'a']=true;
+        }
+
+        for(int i=0; i<26; i++){
+            if(visit[i] == false){
+                return false;
+            }
+        }
+        return true;
+    }
+}
